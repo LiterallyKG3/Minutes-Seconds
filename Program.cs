9dbe9a19095bb97df9f59ded16c288210447e9b4
KG3 <kg360120@yahoo.com>
2022-08-18 15:29:47 +0430

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.ForegroundColor = ConsoleColor.Green;
+          Console.Title = "Minutes:Seconds";
+          Random randomNumber = new Random();
+          
+          int sec;
+          bool game = true;
+          int streak = 0;
+
+          Console.WriteLine("You will be given a random amount of seconds, convert it to Minutes:Seconds\nPress enter to proceed\n----------------------------------------------------------------------");
+          Console.ReadKey();
+
+          while (game == true) {
+           Console.ForegroundColor = ConsoleColor.White; 
+           sec = randomNumber.Next(100,1000);
+           int min = (sec/60);
+           string? answer;
+           string? correct;
+           //"?" makes the strings nullable and removes the annoying problem markers
+           
+           if ((sec - min * 60) > 9) {
+             correct = Convert.ToString(min + ":" + (sec - min * 60));
+           }
+           else {
+             correct = Convert.ToString(min + ":0" + (sec - min * 60));
+           }
+           
+           Console.WriteLine("\n" + sec + "?");
+
+           answer = Console.ReadLine();
+
+           if (answer == correct) {
+            Console.WriteLine("Correct!\n");
+            streak++;
+           }
+           else {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nYou Lose!\nStreak: " + streak + "\nRetry? (Y/N)");
+            game = false;
+            string retry;
+            retry = Console.ReadLine()!; //"!" removes the annoying problem markers
+            if (retry == "Y") {
+              game = true;
+              streak = 0;
+            }
+            if (retry == "N") {
+              Console.ForegroundColor = ConsoleColor.Gray;
+              Console.WriteLine("\nPress Escape to close the game");
+              
+            }
+           }
+          }
+          
+          Console.ReadKey();
+        }
+    }
+}
